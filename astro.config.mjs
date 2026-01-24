40819a96e7600e2271f29823c697fecbb49f9e22
wmxolito <wm@xolito.ai>
2026-01-24 13:29:01 -0600

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   server: { host: true },
+  integrations: [react()],
 });

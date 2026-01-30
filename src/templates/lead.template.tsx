10ceec68d13bd0b633ed58f6842648163376d61a
wmxolito <wm@xolito.ai>
2026-01-29 18:28:17 -0600

    once more update lead template

--- src/templates/lead.template.tsx
+++ src/templates/lead.template.tsx
@@ -26,7 +26,7 @@ export function ContactLeadEmail({
             <div className="relative mb-6 pl-4">
               <div className="absolute left-0 top-1 h-5 w-1 bg-indigo-600" />
               <Heading className="relative pl-4 text-xl sm:text-2xl font-semibold tracking-tight text-neutral-900 before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:h-5 before:w-1 before:bg-indigo-600">
-                <span>Nuevo lead desde el sitio</span>
+                Nuevo lead desde el sitio
               </Heading>
             </div>
 

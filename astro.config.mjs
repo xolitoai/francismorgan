08d7f51aa3ae93bbdd08a29d005552929a755f94
wmxolito <wm@xolito.ai>
2026-01-29 18:21:20 -0600

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -13,4 +15,5 @@ export default defineConfig({
 
   server: { host: true },
   integrations: [react()],
+  adapter: vercel(),
 });
